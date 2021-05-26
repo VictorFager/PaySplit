1bceedc32da6b85db361baf21d3877e037c5bef8
Victor Fager <victorfager@gmail.com>
2021-05-26 17:57:30 +0200

    Setup: Project setup with Firebase tested using cloud firestore

---
+++ app/paysplit/android/app/src/main/kotlin/com/example/paysplit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pixelco.paysplit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app/paysplit/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Firebase Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Firebase'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => FirebaseFirestore.instance
+              .collection('testing')
+              .add({'timestamp': Timestamp.fromDate(DateTime.now())}),
+          child: Icon(Icons.add),
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('testing').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.data == null || snapshot.data.docs == null) return const SizedBox.shrink();
+            return ListView.builder(
+              itemCount: snapshot.data.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                final docData = snapshot.data.docs[index].data();
+                final dateTime = (docData['timestamp'] as Timestamp).toDate();
+                return ListTile(
+                  title: Text(dateTime.toString()),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
